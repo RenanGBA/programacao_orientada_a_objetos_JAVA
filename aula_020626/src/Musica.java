9147853e2ba1cbde27bce619288448dde8930e12
Renan Almeida <125520953+RenanGBA@users.noreply.github.com>
2026-06-08 18:35:25 -0300

    aula 02/06/26 (#5)

---
+++ aula_020626/src/Musica.java
@@ -0,0 +1,17 @@
+public class Musica {
+
+    private String titulo, artista;
+
+    public Musica(String mTitulo, String mArtista) {
+        this.titulo = mTitulo;
+        this.artista = mArtista;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getArtista() {
+        return this.artista;
+    }
+}
